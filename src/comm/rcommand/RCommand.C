15431bf5afb06e5909ae03824b50f06cbbe220cc
CA Ohlin <andre.ohlin@umu.se>
2017-06-15 11:36:20 +0200

    changed RCommand.C

--- src/comm/rcommand/RCommand.C
+++ src/comm/rcommand/RCommand.C
@@ -41,6 +41,7 @@
 #include "comm/expect.h"
 
 #define MAXARGS 32
+/*#define MAXLINE 256*/
 #define MAXLINE 16384
 
 static const char* sshpass_opts[] = {"-o", "PasswordAuthentication=yes",
@@ -172,8 +173,8 @@ bool RCommand::expwrite(const string& command)
 {
   int comlen = command.length();
 
-  if (comlen >= 256) {
-    p_errMessage = "Exceeds maximum C shell command length of 256 characters";
+  if (comlen >= MAXLINE) {
+    p_errMessage = "Exceeds maximum C shell command length of 16384 characters";
     return false;
   }
 
@@ -190,8 +191,8 @@ bool RCommand::expwrite(const char* command)
   int comlen = strlen(command);
 
   if (comlen >= MAXLINE) {
-  	p_errMessage = "Exceeds maximum C shell command length of 16384 characters";
-  return false;
+    p_errMessage = "Exceeds maximum C shell command length of 16384 characters";
+    return false;
   }
 
   if (write(p_fid, command, comlen)==comlen && write(p_fid, "\n", 1)==1)
@@ -245,9 +246,9 @@ bool RCommand::fidwrite(const int& fid, const string& command,
 {
   int comlen = command.length();
 
- if (comlen >= MAXLINE) {
- 	errMessage = "Exceeds maximum C shell command length of 16384 characters";
-  return false;
+  if (comlen >= MAXLINE) {
+    errMessage = "Exceeds maximum C shell command length of 16384 characters";
+    return false;
   }
 
   if (write(fid, command.c_str(), comlen)==comlen && write(fid, "\n", 1)==1)
@@ -3959,7 +3960,9 @@ bool RCommand::shellput(const char** fromFiles, const string& toFile)
       // a 256 character limit for commands to C shell.  The STOP_XFER
       // write is not needed for the sysread/syswrite version because it
       // it reads based on the size of the file.
-	  // Update Anno 2017 -- the modern C shell limit is 64k
+
+	//16 June 2015 -- on modern systems the C shell limit seems to be 64k
+	//or higher
 
       //cmdstat = "perl -e 'open(TMP,\">" + fullToFile + "\");$n=" + nstr + ";$t=0;while ($t<$n){$i=sysread(STDIN,$s,$n-$t);$t=$t+$i;syswrite(TMP,$s,$i);}close(TMP);';echo CMDSTAT=$status";
 
